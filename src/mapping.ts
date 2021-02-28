800026de371466519b33594bda2dd391282fe1ec
Joseph Saw <joesawtw@gmail.com>
2021-02-28 17:19:43 +0800

    disabling handleNewMarketFactory for now

--- src/mapping.ts
+++ src/mapping.ts
@@ -51,7 +51,7 @@ export function handleNewForge(event: NewForgeEvent): void {
   PendleForgeTemplate.create(event.params.forgeAddress);
 }
 
-export function handleNewMarketFactory(event): void {}
+// export function handleNewMarketFactory(event): void {}
 
 /* ** PENDLE FORGE EVENTS */
 export function handleNewYieldContracts(event: NewYieldContractsEvent): void {
